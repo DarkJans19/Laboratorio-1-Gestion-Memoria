771048e0b9cf88471b1615c0039cf4455ff4550b
𝐋𝐈𝐍𝐀 𝐌𝐎𝐑𝐄𝐍𝐎 <lsmorenog@udistrital.edu.co>
2025-09-25 09:42:05 -0500

    Add files via upload

---
+++ lab1 intento1/script.js
@@ -0,0 +1,22 @@
+// Capturar botones
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+
+// Asignar acciones
+b1.addEventListener("click", () => {
+  alert("Elegiste la opción 1");
+});
+
+b2.addEventListener("click", () => {
+  alert("Elegiste la opción 2");
+});
+
+b3.addEventListener("click", () => {
+  alert("Elegiste la opción 3");
+});
+
+b4.addEventListener("click", () => {
+  alert("Elegiste la opción 4");
+});
+
