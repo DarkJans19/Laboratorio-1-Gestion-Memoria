7c65081fa1799521267faf8d46f685a5e8e82e30
𝐋𝐈𝐍𝐀 𝐌𝐎𝐑𝐄𝐍𝐎 <lsmorenog@udistrital.edu.co>
2025-10-04 18:28:57 -0500

    Menús iniciales

--- lab1 intento1/script.js
+++ lab1 intento1/script.js
@@ -1,22 +1,24 @@
-// Capturar botones
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
 
-// Asignar acciones
-b1.addEventListener("click", () => {
-  alert("Elegiste la opción 1");
-});
+const menuParticion = document.getElementById("menu-particion");
+const menuAlgoritmo = document.getElementById("menu-algoritmo");
+const menuAnadirP = document.getElementById("menu-anadirproceso");
+const menuEliminarP = document.getElementById("menu-eliminar-proceso");
 
-b2.addEventListener("click", () => {
-  alert("Elegiste la opción 2");
-});
+const cerrarParticion = document.getElementById("cerrar-particion");
+const cerrarAlgoritmo = document.getElementById("cerrar-algoritmo");
+const cerrarAnadir = document.getElementById("cerrar-anadir");
+const cerrarEliminar = document.getElementById("cerrar-eliminar");
 
-b3.addEventListener("click", () => {
-  alert("Elegiste la opción 3");
-});
+b1.addEventListener("click", () => menuParticion.style.display = "flex");
+b2.addEventListener("click", () => menuAlgoritmo.style.display = "flex");
+b3.addEventListener("click", () => menuAnadirP.style.display = "flex");
+b4.addEventListener("click", () => menuEliminarP.style.display = "flex");
 
-b4.addEventListener("click", () => {
-  alert("Elegiste la opción 4");
-});
-
+cerrarParticion.addEventListener("click", () => menuParticion.style.display = "none");
+cerrarAlgoritmo.addEventListener("click", () => menuAlgoritmo.style.display = "none");
+cerrarAnadir.addEventListener("click", () => menuAnadirP.style.display = "none");
+cerrarEliminar.addEventListener("click", () => menuEliminarP.style.display = "none");
